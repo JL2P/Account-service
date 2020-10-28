2379e4e38bf3f4d79dfda3a0a9f9c9a1a0ad4a11
song <eunsonglee0204@gmail.com>
2020-10-28 19:05:31 +0900

    account

--- src/main/java/com/account/api/domain/logic/AccountServiceImpl.java
+++ src/main/java/com/account/api/domain/logic/AccountServiceImpl.java
@@ -1,9 +1,43 @@
 package com.account.api.domain.logic;
 
+import com.account.api.domain.Account;
 import com.account.api.domain.service.AccountService;
+import com.account.api.repository.AccountRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AccountServiceImpl implements AccountService {
     //AccountService를 구현해야함
+
+    private final AccountRepository accountRepository;
+    public AccountServiceImpl(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+
+    @Override
+    public List<Account> getAccountList(){
+        List<Account> accounts = accountRepository.findAll();
+        return accounts;
+    }
+
+    @Override
+    public Account getAccount(String accountId) {
+        Account account = accountRepository.findById(accountId).orElseThrow();
+        return account;
+    }
+
+    @Override
+    public Account modifyAccount(Account account) {
+        accountRepository.save(account);
+        return account;
+    }
+
+    @Override
+    public void deleteAccount(String accountId) {
+        accountRepository.deleteById(accountId);
+    }
+
 }
--- src/main/java/com/account/api/web/AccountController.java
+++ src/main/java/com/account/api/web/AccountController.java
@@ -1,15 +1,36 @@
 package com.account.api.web;
 
+import com.account.api.domain.Account;
+import com.account.api.domain.service.AccountService;
 import io.swagger.annotations.Api;
-import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @Api(tags = {"1. Account"})
-@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/accounts/") //컨트롤러 기본 URL
 public class AccountController {
     //컨트롤러 구현 시작
+    private final AccountService accountService;
+
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping()
+    public List<Account> getAccounts(){
+        return accountService.getAccountList();
+    }
+
+    @GetMapping("{accountId}/")
+    public Account getAccount(@PathVariable String accountId) {
+        return accountService.getAccount(accountId);
+    }
+
 
 }
