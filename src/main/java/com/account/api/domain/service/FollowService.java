17b5e7efab77bd72e16c360f84a583214e690ae3
parkmm14 <palswo@naver.com>
2020-11-11 22:24:05 +0900

    팔로우 요청한 팔로워 리스트 조회 메서드 서비스, 컨트롤러에 추가

--- src/main/java/com/account/api/domain/service/FollowService.java
+++ src/main/java/com/account/api/domain/service/FollowService.java
@@ -6,8 +6,12 @@ import com.account.api.domain.Following;
 import com.account.api.exception.FollowCheckException;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public interface FollowService {
+
+    //confirm 받지 않은 나의 팔로워들 조회하는 함수
+    public List<Follower> getAllFollowers(String accountId) throws NoSuchElementException;
     //나랑 팔로우관계인지 체크하는 함수.
     public boolean followCheck(String myAccountId1, String myAccountId2) throws FollowCheckException;
     //팔로우 기능
@@ -15,8 +19,8 @@ public interface FollowService {
 
     public void accept(String myAccountId1, String myAccountId2);
     public void refuse(String myAccountId1, String myAccountId2);
-    public List<Following> getFollowings(String myAccountId);
-    public List<Follower> getFollowers(String myAccountId);
-    public int getNumberOfMyFollowings(String myAccountId);
-    public int getNumberOfMyFollowers (String myAccountId);
+    public List<Following> getFollowings(String myAccountId) throws NoSuchElementException;
+    public List<Follower> getFollowers(String myAccountId) throws NoSuchElementException;
+    public int getNumberOfMyFollowings(String myAccountId) throws NoSuchElementException;
+    public int getNumberOfMyFollowers (String myAccountId) throws NoSuchElementException;
 }
